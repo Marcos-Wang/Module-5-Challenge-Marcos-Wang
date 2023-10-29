5f70d67010014b6197c7897296a25ff4e4a83eea
Marcos Wang <wangfmarcos@gmail.com>
2023-10-28 19:08:30 -0700

    installed dayjs and implemented a display for the current date.

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var today = dayjs();
+  var nowEl = $('#currentDay');
+  nowEl.text((today.format('dddd, MMMM D YYYY, h:mm:ss a')));
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
