75b960d7a0e5a7649386cd77cc5c89ae54b0c809
Marcos Wang <wangfmarcos@gmail.com>
2023-10-28 19:41:13 -0700

    Added functionality to tell what hours are in the past, present, and future.

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var today = dayjs();
-  var nowEl = $('#currentDay');
-  nowEl.text((today.format('dddd, MMMM D YYYY, h:mm:ss a')));
 
-  //
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var now = dayjs();
+  var nowHour = now.format('H');
+  for (let i = 9; i <= 17; i++) {
+    let h = "#hour-" + i;
+    if (nowHour - i > 0) {
+      $(h).addClass("past");
+    } else if (nowHour - i == 0) {
+      $(h).addClass("present");
+    } else {
+      $(h).addClass("future");
+    }
+
+
+  }
+
+  $("#hour-9").addClass("past");
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+
+
   // TODO: Add code to display the current date in the header of the page.
+
+
+  var nowEl = $('#currentDay');
+  nowEl.text((now.format('dddd, MMMM D YYYY, h:mm a')));
 });
